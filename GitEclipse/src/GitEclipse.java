34a249c0e0632b5aead6d636156a2463788ecc9f
Sou-123 <soumyadmanuel@gmail.com>
2022-06-28 16:30:29 +0530

    Soumya's commit

--- GitEclipse/src/GitEclipse.java
+++ GitEclipse/src/GitEclipse.java
@@ -3,7 +3,9 @@ public class GitEclipse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Deepesh Rohit");//Deepesh
+		System.out.println("Soumya Manuel");//Soumya
 	}
 
 }
